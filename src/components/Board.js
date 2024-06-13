3d1aa3938477dc727378563b9d368df3bf030316
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-06-13 23:16:55 +0900

    feat : 승자 결정 구현

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,10 +5,47 @@ import "./Board.css";
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
-  const status = `Next player ${xIsNext ? "X" : "O"}`;
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  let status;
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = "Winner :" + winner;
+  } else {
+    status = `Next player ${xIsNext ? "X" : "O"}`;
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
+
+    //승자가 있거나 이미 값이 들어있는 경우 클릭하지 못하게 막기
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
     newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
     setXIsNext(!xIsNext);
@@ -20,7 +57,7 @@ const Board = () => {
 
   return (
     <div>
-      <div className="status">${status}</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
