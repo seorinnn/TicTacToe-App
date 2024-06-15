df3eee9f8c06a3b5ab0f68511a5e317f63fbbbd2
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-06-15 17:02:35 +0900

    feat : 스타일링 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Board from "./components/Board";
+import styles from "./App.css";
 
 function App() {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
@@ -62,7 +63,9 @@ function App() {
     const desc = move ? "Go to move #" + move : "Go to game start";
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
       </li>
     );
   });
@@ -80,7 +83,7 @@ function App() {
       </div>
       <div className="game-info">gameinfo</div>
       <div className="status">{status}</div>
-      <ol>{moves}</ol>
+      <ol style={{ listStyle: "none" }}>{moves}</ol>
     </div>
   );
 }
